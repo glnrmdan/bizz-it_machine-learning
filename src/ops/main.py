6c3270333d04881b2a182188682c785d2d6ee008
irizqy <arizqyakbar@gmail.com>
2023-05-29 14:38:46 +0700

    add cropped image to be process to similiarity model

--- src/ops/main.py
+++ src/ops/main.py
@@ -1,10 +1,13 @@
 from flask import Flask, request
 from PIL import Image
-from tensorflow.saved_model import load
+import tensorflow as tf
 import json
 import numpy as np
 import base64
 import io
+import os
+from os.path import join
+
 
 # logo detector model path
 LD_MODEL_PATH = '/home/irizqy/ml_ws/bangkit-ws/src/logo-detector/efficientnet512_ft/saved_model'
@@ -12,7 +15,7 @@ LD_LABEL_PATH = '/home/irizqy/ml_ws/bangkit-ws/data/label_map.pbtxt'
 
 FR_MODEL_PATH = None
 
-ld_detector = load(LD_MODEL_PATH)
+ld_detector = tf.saved_model.load(LD_MODEL_PATH)
 
 app = Flask(__name__)
 
@@ -27,14 +30,21 @@ def detect_logo():
     img_bytes = base64.b64decode(req.get('data').encode('utf-8'))
     im = Image.open(io.BytesIO(img_bytes))
     im_arr = np.asarray(im)
-    # im_bytes = base64.decodebytes(bytes(req.get('data'), 'utf-8'))
-    # im_arr = np.frombuffer(im_bytes)
+
+    input_tensor = tf.convert_to_tensor(im_arr)
+    input_tensor = input_tensor[tf.newaxis, ...]
+    detections = ld_detector(input_tensor)
+
+    width, height, _ = im_arr.shape
+    print(detections['detection_boxes'][0][0])
+    ymin, xmin, ymax, xmax = detections['detection_boxes'][0][0]
+    (left, right, top, bottom) = (xmin*width, xmax*width, ymin*height, ymax*height)
+    cropped_im = im_arr[int(top):int(bottom), int(left):int(right)]
+    Image.fromarray(cropped_im).save(os.path.join(os.getcwd(), 'cropped-logo.jpg'))
 
     response_json = {
         "data": 'success' 
     }
-
-    print(im_arr)
 
     return json.dumps(response_json)
 
--- src/ops/request_test.py
+++ src/ops/request_test.py
@@ -1,22 +1,19 @@
 import base64
 import requests
-import matplotlib.pyplot as plt
 import json
-import numpy as np
-from PIL import Image
-import io
+
 
 # request data
-im = '/home/irizqy/ml_ws/bangkit-ws/data/test/1_20230510_161105.jpg'
+im = '/home/irizqy/ml_ws/bangkit-ws/data/test/2_20230507_092258.jpg'
 with open(im, "rb") as f:
     im_bytes = f.read()        
 im_b64 = base64.b64encode(im_bytes).decode("utf8")
 headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+print(type(im_b64))
 
 # server get
-
 data =  {
     "data": im_b64
 }
 
-requests.post('http://127.0.0.1:3000/ld_predict', data=json.dumps(data), headers=headers)
+# requests.post('http://127.0.0.1:3000/ld_predict', data=json.dumps(data), headers=headers)
