804efb70eeb16ad704d96655bf04572990dd12ed
irizqy <arizqyakbar@gmail.com>
2023-06-07 00:07:14 +0700

    change how endpoint react for status code 500

--- src/ops/app/main.py
+++ src/ops/app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from PIL import Image
 from app.model import ImageParser
 from app.utils import cropped_detected_im
@@ -20,39 +20,47 @@ ld_detector = tf.saved_model.load(LD_MODEL_PATH)
 
 app = FastAPI()
 
+
 @app.get('/')
 def index():
     return {'greetings': 'Hello World!'}
 
 
+# @app.post('/ld_predict', status_code=500)
+# async def detect_logo_serve_err():
+#     response_json = {
+#         'status_code': 500,
+#         'message': 'Internal Server Error',
+#         }
+#     return json.dumps(response_json)
+
+
 @app.post('/ld_predict', status_code=200)
 async def detect_logo(im: ImageParser):
+    response_json = {
+        'status': 0,
+    }
+
+    img_bytes = base64.b64decode(im.image.encode('utf-8'))
+    im = Image.open(io.BytesIO(img_bytes))
+    im_arr = np.asarray(im)
+
+    # take only RGB values from RGBA images
+    if im_arr.shape[2] == 4:
+        im_arr = im_arr[:, :, :3]
+
+    input_tensor = tf.convert_to_tensor(im_arr)
+    input_tensor = input_tensor[tf.newaxis, ...]
+    detections = ld_detector(input_tensor)
+
+    cropped_ims = cropped_detected_im(detections, im_arr)
 
     response_json = {
-            'status': 400,
-            'message': 'Incorrect data format'
-        }
-    
-    try:
-        img_bytes = base64.b64decode(im.image.encode('utf-8'))
-        im = Image.open(io.BytesIO(img_bytes))
-        im_arr = np.asarray(im)
-
-        # take only RGB values from RGBA images
-        if im_arr.shape[2] == 4:
-            im_arr = im_arr[:,:,:3]
-
-        input_tensor = tf.convert_to_tensor(im_arr)
-        input_tensor = input_tensor[tf.newaxis, ...]
-        detections = ld_detector(input_tensor)
-
-        cropped_ims = cropped_detected_im(detections, im_arr)
-    
-        response_json = {
-            'status': 1,
+        'status_code': 200,
+        'message': 'Ok',
+        'data': {
             'length': len(cropped_ims)
         }
-    except Exception:
-        pass
+    }
 
     return json.dumps(response_json)
